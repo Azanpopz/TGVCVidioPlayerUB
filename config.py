dc922af4ae6b19bb35445cdd4d7c6aa41d78f71b
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-09 15:32:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", "3938951"))
-    API_HASH = os.environ.get("API_HASH", "6561686ba611b2d46efedef7debd6fa5")
-    LOG_ID = int(os.environ.get("LOG_ID", "-1001550013674"))
-    SESSION_STRING = os.environ.get("SESSION_STRING", "BQA2JaRNbwdqRYuWwNdvnrv7jL9vu840jC7cULPlGEtAu_aES9-IKBRvyPh-ek5xU900tTo-pLyqt0-f0usxlLst9gTF5G_3kgx3-AbK4yVO__6C0MlWP2ATcCDPsFR7ZFiicV2-4Bp8ZzwA38UqzBPYbRQkuXY6XtGuMoSnQY_5kv5qck60qQELLcqXUZaFiRN2aIOxirnfmEvwElLu6GtRnUWYbTBCT7sfUyU3C7wPoAs5h1K1cNJH9yT1OuDvsfbZqAblKPdiNI4SfAp9JW_RTPZf9-bqUoL98T46kSlK04Fl1gfbp2BcLiXGQRpGHKUQHhhfzq79fVidGV_lKF20SM-CxQA")
+    API_ID = int(os.environ.get("API_ID", "1778836"))
+    API_HASH = os.environ.get("API_HASH", "7bcf61fcd32b8652cd5876b02dcf57ae")
+    LOG_ID = int(os.environ.get("LOG_ID", -1001565141207"))
+    SESSION_STRING = os.environ.get("SESSION_STRING", "BQA8Omi7tl41FSWCOa5PFzSQrZqdypRTx5mq7oLULKDYplnPNrUeL5nC-luRC4A1V5BRVDeEKHOwjCJpNrvvstOlRV2gXPttxVyCknunSkxhQHK_aUQgM_prv3enFlBj1yfdjzyypGCOhaRKpQuM5y3RKmMlQ-w2aAL9noUyjDA1BuVY5YC2ZcxAWcz3lALmnC09P4BA0ANPHuArI8Bd6mZeP4AZb0mRhynnejmWL9pM3iW2pe2KRjpt54W-Q1vXZTGNVf1uzx7Z0iJNSPFa3YVOkgwoeUYgLE3VKH2imU3Fi4wtURMGVmWZAu3udl_xvA-S1cJn23ha7XJ2mCyfKfA0TQHpigA")
 
 class Database:
     VIDEO_CALL = {}
